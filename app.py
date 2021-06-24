879c2571124faef08fc5f6b233e70ba7a1aa9cbf
yun <sys394880@gmail.com>
2021-06-24 16:26:38 +0900

    flash message

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort, session, redirect, url_for
+from flask import Flask, render_template, abort, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -16,15 +16,14 @@ app.config['SECRET_KEY'] = 'hard secret key token'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # name = None
     form = NameForm()
     if form.validate_on_submit():
-        # name = form.name.data
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         form.name.data = ''
-        print("URL : " + url_for('login'))
-        return redirect(url_for('login'))
-    # return render_template('index.html', form = form, name = name)
+        return redirect(url_for('index'))
     return render_template('index.html', form = form, name = session.get('name'))
 
 @app.route('/name/<test>')

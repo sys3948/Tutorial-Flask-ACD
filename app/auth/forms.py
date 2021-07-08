63606aee6698fd7518957b124e40d11edb387901
yun <sys394880@gmail.com>
2021-07-08 22:21:36 +0900

    비밀번호 변경 & 찾기 기능 추가 구현

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,5 @@
+from flask.globals import request
+from wtforms import validators
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import Required, Email, Length, Regexp, EqualTo
@@ -17,3 +19,27 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[Required(), EqualTo('password2', message='Passwords must match.')])
     password2 = PasswordField('Confirm password', validators=[Required()])
     submit = SubmitField('Register')
+
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField('Old Password', validators=[Required()])
+    password = PasswordField('New Password Field', validators=[Required(), EqualTo('password2', message='Password must match.')])
+    password2 = PasswordField('Confirm new Password', validators=[Required()])
+    submit = SubmitField('Update Password')
+
+
+class PasswordResetRequestForm(FlaskForm):
+    email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
+    submit = SubmitField('Reset Password')
+
+
+class PasswordResetForm(FlaskForm):
+    password = PasswordField('Reset Password', validators=[Required(), EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('Confirm Password', validators=[Required()])
+    submit = SubmitField('Reset Password')
+
+
+class ChangeEmailForm(FlaskForm):
+    email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
+    password = PasswordField('Password', validators=[Required()])
+    submit = SubmitField('Update Email Address')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, request, url_for, flash, current_app, session
 from wtforms.validators import Email
 from . import auth
-from .forms import LoginForm, RegistrationForm
+from .forms import LoginForm, RegistrationForm, ChangePasswordForm, PasswordResetForm, PasswordResetRequestForm
 from ..email import send_email
 import pymysql
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -82,9 +82,9 @@ def confirm(token):
 
 @auth.before_app_request
 def before_request(): # request hooks - before_request : 각 리퀘스트 전에 실행하는 함수를 등록하는 후크이다. before_request 와 같은 후크로 blueprint를 적용할 시 before_request 후크는 before_app_request로 변경된다.
-    print('request hooks!!')
-    print('request End Point! : ' + request.endpoint) # 현재 URL에 해당하는 뷰함수의 포인트를 출력 ex) main/views.py의 index 뷰함수면 main.index
-    print('request Blueprint! : ' + request.blueprint) # 현재 URL에 해당하는 blueprint의 명칭을 출력.
+    # print('request hooks!!')
+    # print('request End Point! : ' + request.endpoint) # 현재 URL에 해당하는 뷰함수의 포인트를 출력 ex) main/views.py의 index 뷰함수면 main.index
+    # print('request Blueprint! : ' + request.blueprint) # 현재 URL에 해당하는 blueprint의 명칭을 출력.
     if 'id' in session and 'name' in session and request.blueprint != 'auth': # user 인증 확인 조건문으로 auth blueprint를 제외(로그인, 회원가입, 재인증, 인증 등에 엮이지 않도록 하기 위한 조건문 내용)
         conn = pymysql.connect(host='192.168.111.133', port=3306, user=current_app.config['DB_USER'], passwd=current_app.config['DB_PASSWD'], database='flasky')
         cur = conn.cursor()
@@ -112,3 +112,89 @@ def resend_confirmation():
     print('send Email!!')
     flash('A new confirmation email has been sent to you by email.')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/change-password', methods=['GET','POST'])
+def change_password():
+    if not 'name' in session and not 'id' in session:
+        flash('로그인을 해주세요.')
+        return redirect(url_for('auth.login'))
+    form = ChangePasswordForm()
+    if form.validate_on_submit(): # submit 버튼을 클릭 했을 때 해당 조건문은 True가 된다.
+        conn = pymysql.connect(host='192.168.111.133', port=3306, user=current_app.config['DB_USER'], passwd=current_app.config['DB_PASSWD'], database='flasky')
+        cur = conn.cursor()
+        cur.execute('select password_hash from user where id = "%s"' %(session.get('id')))
+        user = cur.fetchone()
+        if user and check_password_hash(user[0], form.old_password.data): # 위에서 조회한 페스워드 해쉬 값과 사용자가 입력한 현재 비밀벊호가 맞는지 확인하고 서브밋한 사용자 계정의 존재 유무를 확인하는 조건문이다.
+            cur.execute("update user set password_hash = '%s' where id = '%s'" %(generate_password_hash(form.password.data), session.get('id'))) # 조건문이 True이면 사용자가 변경한 비밀번호 값을 werkzeug.security 모듈의 generate_password_hash 함수로 해싱을 하여 DB에 Update한다.
+            conn.commit()
+            flash('Your password has been updated')
+            cur.close()
+            conn.close()
+            return redirect(url_for('main.index'))
+        else:
+            flash('Invalid password')
+        cur.close()
+        conn.close()
+    return render_template('auth/change_password.html', form = form)
+
+
+@auth.route('/reset', methods=['GET', 'POST'])
+def password_reset_required():
+    # password를 잊었을 때 찾기 위한 뷰함수로 로그인이 되어있지 않는 상태여야한다.
+    if 'name' in session and 'id' in session:
+        flash('잘 못 접근하셨습니다.')
+        return redirect('main.index')
+    form = PasswordResetRequestForm()
+    if form.validate_on_submit():
+        # reset password 요청 폼이 submit되었을 때 해당 이메일로 토큰을 전송해야한다.
+        # 토큰 전송할 값은 id 컬럼 값이다.
+        conn = pymysql.connect(host='192.168.111.133', port=3306, user=current_app.config['DB_USER'], passwd=current_app.config['DB_PASSWD'], database='flasky')
+        cur = conn.cursor()
+        cur.execute('select id, username from user where email = "%s"' %(form.email.data))
+        token_id = cur.fetchone()
+        cur.close()
+        conn.close()
+        s = Serializer(current_app.config['SECRET_KEY'], 3600)
+        token = s.dumps({'reset' : token_id[0]})
+        send_email(form.email.data, 'Reset Your Password', 'auth/email/reset_password',username = token_id[1], token = token)
+        return redirect(url_for('auth.login'))
+    return render_template('auth/reset_password.html', form=form)
+
+
+@auth.route('/reset/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if 'name' in session and 'id' in session:
+        flash('잘 못 된 접근 입니다.')
+        return redirect(url_for('main.index'))
+
+    form = PasswordResetForm()
+
+    if form.validate_on_submit():
+        s = Serializer(current_app.config['SECRET_KEY'], 3600)
+        conn = pymysql.connect(host='192.168.111.133', port=3306, user=current_app.config['DB_USER'], passwd=current_app.config['DB_PASSWD'], database='flasky')
+        cur = conn.cursor()
+        try:
+            data = s.loads(token)
+            print(data)
+            cur.execute("select id from user where id = '%s'" %(data.get('reset')))
+            user_info = cur.fetchone()
+            if not user_info:
+                cur.close()
+                conn.close()
+                flash('잘 못 된 정보입니다.')
+                return redirect(url_for('main.index'))
+            cur.execute("update user set password_hash = '%s' where id = '%s'" %(generate_password_hash(form.password.data), user_info[0]))
+            conn.commit()
+            cur.close()
+            conn.close()
+            flash('Your password has been update.')
+            return redirect(url_for('auth.login'))
+        except Exception as e:
+            cur.close()
+            conn.close()
+            print('에러 발생! 에러 내용 : ' + str(e))
+            flash('에러가 발생했습니다.')
+            return redirect(url_for('main.index'))
+
+    return render_template('auth/reset_password.html', form = form)
